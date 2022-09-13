0fd17801c5c38d9a1241f79d4ecc0a1cea4f8d91
Julian Marotta <julianmarotta@gmail.com>
2022-09-13 11:45:09 -0400

    Add rg

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,6 +15,13 @@ vim.api.nvim_set_keymap(
   { noremap = true }
 )
 
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-F>",
+  ":Rg<CR>",
+  { noremap = true }
+)
+
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
