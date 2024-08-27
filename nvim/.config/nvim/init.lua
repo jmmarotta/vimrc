e694e2ce349c8dca835f84c8a28ccf9ffacc40bf
Julian Marotta <julian@equisolve.com>
2024-08-27 16:26:58 -0500

    Add avante

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -981,6 +981,85 @@ require("lazy").setup({
   --     vim.keymap.set("n", "<leader>ar", "<cmd>lua ReloadBuffer()<cr>", { desc = "[A]ider [R]eload" })
   --   end,
   -- },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    opts = {
+      -- add any opts here
+      --provider = "claude",
+      --claude = {
+      --  endpoint = "https://api.anthropic.com",
+      --  model = "claude-3-5-sonnet-20240620",
+      --  temperature = 0,
+      --  max_tokens = 4096,
+      --},
+      --mappings = {
+      --  ask = "<leader>aa",
+      --  edit = "<leader>ae",
+      --  refresh = "<leader>ar",
+      --  --- @class AvanteConflictMappings
+      --  diff = {
+      --    ours = "co",
+      --    theirs = "ct",
+      --    none = "c0",
+      --    both = "cb",
+      --    next = "]x",
+      --    prev = "[x",
+      --  },
+      --  jump = {
+      --    next = "]]",
+      --    prev = "[[",
+      --  },
+      --  submit = {
+      --    normal = "<CR>",
+      --    insert = "<C-s>",
+      --  },
+      --  toggle = {
+      --    debug = "<leader>ad",
+      --    hint = "<leader>ah",
+      --  },
+      --},
+      --hints = { enabled = true },
+      --windows = {
+      --  wrap = true, -- similar to vim.o.wrap
+      --  width = 30, -- default % based on available width
+      --  sidebar_header = {
+      --    align = "center", -- left, center, right for title
+      --    rounded = true,
+      --  },
+      --},
+      --highlights = {
+      --  ---@type AvanteConflictHighlights
+      --  diff = {
+      --    current = "DiffText",
+      --    incoming = "DiffAdd",
+      --  },
+      --},
+      ----- @class AvanteConflictUserConfig
+      --diff = {
+      --  debug = false,
+      --  autojump = true,
+      --  ---@type string | fun(): any
+      --  list_opener = "copen",
+      --},
+    },
+    dependencies = {
+      --"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "echasnovski/mini.icons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
