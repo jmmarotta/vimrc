e016755104a1655f449de2e723529bccf78b84fd
Julian Marotta <julianmmarotta@gmail.com>
2025-02-09 12:17:18 -0500

    Add keymaps for CC inline

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1127,6 +1127,16 @@ require("lazy").setup({
         },
         inline = {
           adapter = "anthropic",
+          keymaps = {
+            accept_change = {
+              modes = { n = "ga" },
+              description = "Accept the suggested change",
+            },
+            reject_change = {
+              modes = { n = "gr" },
+              description = "Reject the suggested change",
+            },
+          },
         },
       },
       log_level = "DEBUG",
